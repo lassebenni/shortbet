0075b95997ace94a43e6a97d78258bbed41a2b58
Lasse Benninga <lassebenninga@godatadriven.com>
2022-01-16 21:13:48 +0100

    Write results to json instead of CSV

--- process_tickers.py
+++ process_tickers.py
@@ -1,56 +1,49 @@
 import concurrent.futures
+import json
 import time
-from typing import Generator
+from typing import Generator, List
 
-from short_ticker import ShortTicker
+from model.short_ticker import ShortTicker
+
+SYMBOLS_PATH = "symbols.txt"
+LATEST_JSON_PATH = "data/latest.json"
 
 
 class ProcessTickers:
     def __init__(
         self,
-        symbols_path: str = "symbols.txt",
-        history_path: str = "data/history.csv",
-        latest_path: str = "data/latest.csv",
     ):
+        self.symbols = self._read_symbols(SYMBOLS_PATH)
+        self.existing_latest_tickers = self._read_tickers()
+        self.latest_tickers: List[ShortTicker] = []
 
-        self.history_filename = history_path
-        self.latest_filename = latest_path
-
-        self.symbols = self._read_symbols(symbols_path)
-
-    def run(self, full_run: bool = False, multi_process: bool = True):
+    def run(self, full_run: bool = False):
         start = time.time()
         first_symbol = next(self.symbols)
         self._store_first_symbol(first_symbol)
 
-        if full_run:
-            print("Processing all symbols")
-            if multi_process:
-                self._multi_process(self.symbols)
-            else:
-                for symbol in self.symbols:
-                    self._store_ticker(symbol)
+        if not full_run:
+            print("Done processing.")
+        else:
+            print("Starting to process all symbols")
+            with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+                executor.map(self._store_ticker, self.symbols)
+            self._overwrite_tickers_file()
 
         self._print_duration(start, "extract_tickers")
-
-    def _multi_process(self, symbols: Generator):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-            executor.map(self._store_ticker, symbols)
 
     def _store_first_symbol(self, symbol: str):
         try:
             ticker = ShortTicker(symbol)
-            ticker.to_csv(self.latest_filename, skip_header=False, append=False)
+            self.latest_tickers.append(ticker)
+            self._overwrite_tickers_file()
         except ValueError:
             print(f"Could not store {symbol}")
 
     def _store_ticker(self, symbol: str):
         try:
             ticker = ShortTicker(symbol)
-            if ticker.earnings_date:
-                ticker.to_csv(self.latest_filename, skip_header=True)
-                ticker.to_csv(self.history_filename, skip_header=True)
-                ticker.print()
+            self.latest_tickers.append(ticker)
         except ValueError:
             print(f"Could not store {symbol}")
 
@@ -65,3 +58,15 @@ class ProcessTickers:
                 symbol = chunk[0].decode("utf-8").strip()
                 yield symbol
                 chunk = file.readlines(buffer_size)
+
+    def _read_tickers(self):
+        with open(LATEST_JSON_PATH, "r") as file:
+            return json.loads(file.read())
+
+    def _update_tickers(self, filename: str):
+        pass
+
+    def _overwrite_tickers_file(self):
+        with open(LATEST_JSON_PATH, "w") as file:
+            tickers_json = [ticker.as_dict() for ticker in self.latest_tickers]
+            json.dump(tickers_json, file, default=str)
