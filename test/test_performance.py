864f2b23dbcd31d7320da625a2bf87d5f9ae1352
Lasse Benninga <lassebenninga@godatadriven.com>
2022-01-16 21:19:09 +0100

    Remove unnecessary test

--- test/test_performance.py
+++ test/test_performance.py
@@ -3,7 +3,7 @@ import pytest
 import pandas as pd
 
 import process_tickers as pts
-from process_tickers import ShortTicker
+from process_tickers import ShortTicker, SYMBOLS_PATH, LATEST_JSON_PATH
 
 
 class FakeTicker:
@@ -16,34 +16,3 @@ class FakeTicker:
 def return_delayed_fake_ticker(s, x):
     time.sleep(5)
     return FakeTicker()
-
-
-@pytest.mark.parametrize(
-    "full_run, multi_process, expected_duration",
-    [(False, False, 10), (True, True, 15), (True, False, 60)],
-)
-def test_process_tickers_performance(
-    tmpdir, full_run, multi_process, expected_duration
-):
-    """
-    Test the performance of the ProcessTickers function.
-    """
-
-    # override networking call
-    ShortTicker._retrieve_yf_ticker = return_delayed_fake_ticker
-
-    hist_path = tmpdir.join("history.csv")
-    latest_path = tmpdir.join("latest.csv")
-
-    pt = pts.ProcessTickers(
-        symbols_path="test/test_symbols.txt",
-        history_path=hist_path,
-        latest_path=latest_path,
-    )
-
-    # Test the performance of the ProcessTickers function
-    start = time.time()
-    pt.run(full_run, multi_process)
-    duration = time.time() - start
-    print(duration)
-    assert duration < expected_duration
